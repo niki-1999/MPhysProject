0c34063c54b0c0fd5fb2f0a238d257dbb449fb55
Nikita Heer <nh3g18@soton.ac.uk>
2021-10-28 15:56:44 +0100

    Initial commit

---
+++ projecttest.py
@@ -0,0 +1,11 @@
+import datetime
+import numpy as np
+import tsyganenko as tsy
+
+trace_dt = datetime.datetime(2001,9,22,12)
+lats = np.arange(10, 90, 10)
+lons = 180.
+rhos = tsy.RE
+trace = tsy.Trace(lats, lons, rhos, datetime=trace_dt)
+
+print(trace)
